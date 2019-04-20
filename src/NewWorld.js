e4c78749a668ec8de83715a46037cc60d3e792c9
Travis Hahn <tjxhahn@gmail.com>
2019-04-20 00:44:07 -0400

    Update NewWorld.js

--- src/NewWorld.js
+++ src/NewWorld.js
@@ -22,7 +22,8 @@ let world = {
     uranium: null,
     thorium: null,
     potassium: null
-  }
+  },
+  grid: [[], [], [], [], []]
 };
 
 world.numberOfTrees = Math.ceil(Math.sqrt((10 + world.size)**4**2));
@@ -40,6 +41,36 @@ Object.keys(world.preciousMetals).forEach(metal => {
 Object.keys(world.radioactive).forEach(iso => {
   world.radioactive[iso] = Math.floor((Math.ceil(Math.sqrt((10 + world.size)**4**2))) / (Math.floor(Math.random() * 300) + 200));
 });
+
+for(let i = 0;i < 5;i++) {
+  for(let j = 0;j < world.size * 5;j++) {
+    let gridFactor = world.size * 5; //This number will be used to handle the distrubution of resources throughout the grid
+    let currentGridSquare = {
+      numberOfTrees: null,
+      animalLife: null,
+      soilFertility: null,
+      ores: {
+        copper: null,
+        lead: null,
+        nickle: null,
+        tin: null,
+        aluminum: null,
+        zink: null,
+        iron: null
+      },
+      preciousMetals: {
+        gold: null,
+        silver: null,
+        platinum: null
+      },
+      radioactive: {
+        uranium: null,
+        thorium: null,
+        potassium: null
+      }
+    };
+  }
+}
 
 document.getElementById("data").innerHTML = JSON.stringify(world);
 };
