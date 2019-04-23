afdc4003f0e966d5064b71f05ccf5a0e347d18ce
Travis Hahn <tjxhahn@gmail.com>
2019-04-23 15:32:08 -0400

    Update NewWorld.js

--- src/NewWorld/NewWorld.js
+++ src/NewWorld/NewWorld.js
@@ -14,6 +14,10 @@ window.onload = () => {
                                                     
     document.getElementById("genUniverse").addEventListener("click", () => {
         planetData = generatePlanet();
-        document.getElementById("data").innerHTML = planetData;
+        document.getElementById("data").innerHTML = JSON.stringify(planetData);
+        
+        //Hide both the genUniverse button and the load file butotn
+        document.getElementById("loadData").style.display = "none";
+        document.getElementById("genUniverse").style.display = "none";
     });
 };
